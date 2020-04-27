4669af331386d29bc7595b268bc6c61fdf7783ad
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-04-27 09:38:36 -0500

    small update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -29,7 +29,7 @@ export const Login = (props) => {
   return (
     <div className="bg-gray-300 h-screen">
       <form onSubmit={handleSubmit} className="border border-blue-800 rounded-lg overflow-hidden w-1/3 mx-auto login-position">
-        <h1 className="text-3xl font-bold text-white p-4 bg-blue-800 tracking-wide">{isRegistration ? "Register" : "Login"}</h1>
+        <h1 className="text-3xl font-bold text-white p-4 bg-blue-800 tracking-wide bg-white">{isRegistration ? "Register" : "Login"}</h1>
 
         <div className="p-4 bg-white">
           <input type="text" name="username" placeholder="Username" onChange={handleChange} value={user.username} className="block p-3 text-xl w-4/5 my-3 mx-auto rounded-lg focus:bg-white focus:outline-none focus:shadow-outline border-blue-600 border"/>        
@@ -41,10 +41,10 @@ export const Login = (props) => {
           }
 
           <button type="submit" className="w-1/5 my-3 border-blue-600 border rounded-lg bg-blue-600 text-white text-xl font-bold py-3 mx-auto block shadow-md hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:shadow-outline">{isRegistration ? "Register" : "Login"}</button>
-        </div>
-        <div>
-          <button onClick={changeForm} id="registration">Register</button>
-          <button onClick={changeForm} id="login">Login</button>
+          <div>
+            <button onClick={changeForm} id="registration" className="w-1/4 p-3 border">Register</button>
+            <button onClick={changeForm} id="login" className="w-1/4 p-3 border">Login</button>
+          </div>
         </div>
       </form>
     </div>
