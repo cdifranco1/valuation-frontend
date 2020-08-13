42dceac5f61cff518d9ca360241b9a8ef4aa08a4
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-08-13 00:28:32 -0500

    restructured folder

---
+++ build/precache-manifest.6d66e62d33cab84ac3f221ae35b042bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5a0fda750eca471b8b9b7cb3c2f36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f59bd8990172b569f5",
+    "url": "/static/css/main.2ba4eedb.chunk.css"
+  },
+  {
+    "revision": "ca9de657a8de846c82ce",
+    "url": "/static/js/2.73a97a67.chunk.js"
+  },
+  {
+    "revision": "ebc5d087fc4c4229c0680561aa363996",
+    "url": "/static/js/2.73a97a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f59bd8990172b569f5",
+    "url": "/static/js/main.dc87655e.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
